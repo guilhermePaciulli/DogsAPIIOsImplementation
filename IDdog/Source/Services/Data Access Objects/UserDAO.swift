2c54ad2e3a346e401b6edffbdf08b0a4f6ea5007
Guilherme Paciulli <guilherme.paciulli@icloud.com>
2018-12-02 01:55:12 -0200

    folder and file

---
+++ IDdog/Source/Services/Data Access Objects/UserDAO.swift
@@ -0,0 +1,16 @@
+//
+//  UserDAO.swift
+//  IDdog
+//
+//  Created by Guilherme Paciulli on 02/12/18.
+//  Copyright © 2018 Guilherme Paciulli. All rights reserved.
+//
+
+import Foundation
+
+class UserDAO {
+    
+    
+    
+    
+}
